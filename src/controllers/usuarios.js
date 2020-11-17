2c71a8deb7df75868f1b0607609b28cefcf16fd1
Thauan Costa <thauan.dcosta@gmail.com>
2020-11-16 22:31:33 -0300

    feat: add encrypt function

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,18 +1,28 @@
 const TabelaUsuarios = require('../repositories/tabelaUsuarios');
 const response = require('../utils/response');
+const Password = require('../utils/password');
 
 const adicionarUsuario = async (ctx) => {
+	const emails = new Set();
 	TabelaUsuarios.criarTabelaUsuario();
 	const { email = null, senha = null, nome = null } = ctx.request.body;
 	if (!email || !senha || !nome) {
 		return response(ctx, 400, { mensagem: 'Mal formatado' });
 	}
+	if (emails.has(email)) {
+		console.log('not today');
+	} else {
+		emails.add(email);
+	}
+	const senhaEncriptada = await Password.encrypt(senha);
+	console.log(senhaEncriptada);
 	const result = await TabelaUsuarios.adicionarUsuarioNaTabela(
 		email,
-		senha,
+		senhaEncriptada,
 		nome
 	);
-	return response(ctx, 201, result.rows.shift());
+	const { id } = result.rows.shift();
+	return response(ctx, 201, `{id: ${id}}`);
 };
 
 module.exports = { adicionarUsuario };
--- src/utils/password.js
+++ src/utils/password.js
@@ -1,8 +1,19 @@
 const bcrypt = require('bcryptjs');
 
+/**
+ * Nessa função, é encriptada a senha e adicionado o sal.
+ */
+const encrypt = async (password) => {
+	const hash = await bcrypt.hash(password, 10);
+	return hash;
+};
+
+/**
+ * Essa função faz a comparação da senha enviada com o hash dela.
+ */
 const check = async (password, hash) => {
 	const comparison = await bcrypt.compare(password, hash);
 	return comparison;
 };
 
-module.exports = { check };
+module.exports = { check, encrypt };
