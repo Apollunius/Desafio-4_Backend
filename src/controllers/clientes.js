e235b694cbc4046d87927eea3f5dfc3864595ff0
Thauan Costa <thauan.dcosta@gmail.com>
2020-11-19 02:42:06 -0300

    feat: search and edit clients

--- src/controllers/clientes.js
+++ src/controllers/clientes.js
@@ -1,7 +1,6 @@
 const TabelaClientes = require('../repositories/tabelaClientes');
 const TabelaUsuarios = require('../repositories/tabelaUsuarios');
 const response = require('../utils/response');
-// const Password = require('../utils/password');
 
 const adicionarCliente = async (ctx) => {
 	const {
@@ -15,10 +14,7 @@ const adicionarCliente = async (ctx) => {
 		return response(ctx, 400, { mensagem: 'Mal formatado' });
 	}
 	const resultDados = await TabelaClientes.localizarCPF(cpf);
-	console.log(await resultDados.rows);
-	console.log((await resultDados.rows) !== []);
 	if (resultDados.rows.length > 0) {
-		console.log('passou aqui?');
 		const cpfCliente = resultDados.rows[0].cpf;
 		if (cpfCliente === cpf) {
 			return response(ctx, 401, {
@@ -29,8 +25,7 @@ const adicionarCliente = async (ctx) => {
 	const resultDados2 = await TabelaUsuarios.localizarId(idUsuario);
 	if (resultDados2.rows.length > 0) {
 		const idUser = resultDados2.rows[0].id;
-		console.log(idUser);
-		if (idUsuario !== idUser) {
+		if (idUsuario !== idUser.toString()) {
 			return response(ctx, 400, { mensagem: 'Mal formatado' });
 		}
 	}
@@ -41,8 +36,41 @@ const adicionarCliente = async (ctx) => {
 		tel,
 		idUsuario
 	);
-	console.log(result.rows);
 	const { id } = await result.rows.shift();
 	return response(ctx, 201, { id: `${id}` });
 };
-module.exports = { adicionarCliente };
+
+const atualizarCliente = async (ctx) => {
+	const {
+		id = null,
+		nome = null,
+		cpf = null,
+		email = null,
+		tel = null,
+	} = ctx.request.body;
+	if (!nome || !cpf || !email || !tel || !id) {
+		return response(ctx, 400, { mensagem: 'Mal formatado' });
+	}
+	const result = await TabelaClientes.atualizarCliente(
+		id,
+		nome,
+		cpf,
+		email,
+		tel
+	);
+	return response(ctx, 200, result.rows);
+};
+const querystring = async (ctx) => {
+	const { offset } = ctx.query;
+	if (ctx.querystring.includes('busca')) {
+		const { busca } = ctx.query;
+		const result = await TabelaClientes.listarClientesPorBusca(
+			busca,
+			offset
+		);
+		return response(ctx, 200, { clientes: result.rows });
+	}
+	const result = await TabelaClientes.listarClientes(offset);
+	return response(ctx, 200, { clientes: result.rows });
+};
+module.exports = { adicionarCliente, atualizarCliente, querystring };
