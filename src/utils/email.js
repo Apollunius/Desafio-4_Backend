82ab25b986c37edfa9979b96149dd553cd63d231
Thauan Costa <thauan.dcosta@gmail.com>
2020-11-19 23:31:51 -0300

    feat: send confirmation email

---
+++ src/utils/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+require('dotenv').config();
+
+const config = {
+	host: process.env.MAILTRAP_HOST,
+	port: process.env.MAILTRAP_PORT,
+	secure: false,
+	auth: {
+		user: process.env.MAILTRAP_USER,
+		pass: process.env.MAILTRAP_PASS,
+	},
+};
+
+const transport = nodemailer.createTransport(config);
+
+const enviarEmail = async (to, subject, html) => {
+	const email = await transport.sendMail({
+		from: '"Tchawan" <thau@cubos.academy>',
+		to,
+		subject,
+		html,
+	});
+	return email;
+};
+
+module.exports = { enviarEmail };
